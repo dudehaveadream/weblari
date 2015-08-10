c1ad244e01f2ccfa416c3e3862a6b16b6cc6201b
Andrew Koval <heyandydude@gmail.com>
2015-08-10 17:53:47 +0300

    website elements

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,11 @@
 $('#toggleSearch').click(function(){
 	$('.search-holder .search-inner').slideToggle();
 });
+
+// Subheader collapse func
+$('.subheader-collapse #toggleSubheader').click(function(e){
+	$(this).parent().hide();
+	$('.subheader-collapse .subheader-info-inner').show();
+	$('.subheader-collapse').addClass('collapsed');
+	e.preventDefault();
+});
