232380369d72fe94a1012c9ed13892b95bf116a7
Andrew Koval <heyandydude@gmail.com>
2015-08-11 09:09:59 +0300

    clean

--- js/custom.js
+++ js/custom.js
@@ -12,3 +12,16 @@ $('.subheader-collapse #toggleSubheader').click(function(e){
 	$('.subheader-collapse').addClass('collapsed');
 	e.preventDefault();
 });
+
+// Likes
+$('.like-link, .like-btn').click(function(e){
+	e.preventDefault();
+});
+
+// Anchor
+$('a[href^="#go"]').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+});
